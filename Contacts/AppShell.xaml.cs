ef99153637b11fc0ed0dd494f0b8dc0e43f12215
rushi-smb <rushi@smbios.co.in>
2023-09-29 16:51:45 +0530

    Add project files.

---
+++ Contacts/AppShell.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Contacts.Views;
+
+namespace Contacts
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
