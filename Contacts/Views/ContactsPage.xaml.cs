3d62ae71d95e2acae7fd0705e6c81aa320ff76ba
rushi-smb <rushi@smbios.co.in>
2023-09-29 17:40:29 +0530

    Add List View and data binding

--- Contacts/Views/ContactsPage.xaml.cs
+++ Contacts/Views/ContactsPage.xaml.cs
@@ -5,15 +5,15 @@ public partial class ContactsPage : ContentPage
 	public ContactsPage()
 	{
 		InitializeComponent();
+
+		List<string> contacts = new List<string>
+		{
+			"John Doe",
+			"Jane Doe",
+			"Tom Hanks",
+			"Frank Liu"
+		};
+
+		listContacts.ItemsSource = contacts;
 	}
-
-    private void btnEditContact_Clicked(object sender, EventArgs e)
-    {
-        Shell.Current.GoToAsync(nameof(EditContactPage));
-    }
-
-    private void btnAddContact_Clicked(object sender, EventArgs e)
-    {
-        Shell.Current.GoToAsync(nameof(AddContactPage));
-    }
 }
