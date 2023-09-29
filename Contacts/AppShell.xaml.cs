c4cf3896994e3cd3d6f162832cf823fac1abf527
rushi-smb <rushi@smbios.co.in>
2023-09-29 17:03:44 +0530

    Register Page Routing

--- Contacts/AppShell.xaml.cs
+++ Contacts/AppShell.xaml.cs
@@ -7,6 +7,10 @@ namespace Contacts
         public AppShell()
         {
             InitializeComponent();
+
+            Routing.RegisterRoute(nameof(ContactsPage), typeof(ContactsPage));
+            Routing.RegisterRoute(nameof(EditContactPage), typeof(EditContactPage));
+            Routing.RegisterRoute(nameof(AddContactPage), typeof(AddContactPage));
         }
     }
 }
