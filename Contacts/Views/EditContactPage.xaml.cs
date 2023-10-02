c5c85447ec467a03251f89c5fb6ea8ec5c2cde24
rushi-smb <rushi@smbios.co.in>
2023-10-02 10:00:38 +0530

    Horizontal & Vertical Stack Layout

--- Contacts/Views/EditContactPage.xaml.cs
+++ Contacts/Views/EditContactPage.xaml.cs
@@ -13,18 +13,18 @@ public partial class EditContactPage : ContentPage
 		InitializeComponent();
 	}
 
-    private void btnCancel_Clicked(object sender, EventArgs e)
-    {
-        //Shell.Current.GoToAsync($"//{nameof(ContactsPage)}");
-        Shell.Current.GoToAsync("..");
-    }
+    //private void btnCancel_Clicked(object sender, EventArgs e)
+    //{
+    //    //Shell.Current.GoToAsync($"//{nameof(ContactsPage)}");
+    //    Shell.Current.GoToAsync("..");
+    //}
 
     public string ContactId
     {
         set
         {
             contact = ContactRepository.GetContactById(int.Parse(value));
-            lblName.Text = contact.Name;
+            // lblName.Text = contact.Name;
         }
     }
 }
