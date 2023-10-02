385c69ac62eb468130b92e6aa0d27656b820f61e
rushi-smb <rushi@smbios.co.in>
2023-10-02 10:10:32 +0530

    Use stacklayout & Frame for Edit Contact Page

--- Contacts/Models/Contact.cs
+++ Contacts/Models/Contact.cs
@@ -11,5 +11,7 @@ namespace Contacts.Models
         public int ContactId { get; set; }
         public string Name { get; set; }
         public string Email { get; set; }
+        public string Phone { get; set; }
+        public string Address { get; set; }
     }
 }
