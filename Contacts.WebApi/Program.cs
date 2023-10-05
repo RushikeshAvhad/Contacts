c42b87254bad9bcb8ec0a4a92feae7f6573e3787
rushi-smb <rushi@smbios.co.in>
2023-10-05 14:12:37 +0530

    Get Contacts Endpoint

--- Contacts.WebApi/Program.cs
+++ Contacts.WebApi/Program.cs
@@ -11,6 +11,13 @@ var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 
-app.UseHttpsRedirection();
+//app.UseHttpsRedirection();  // If we try to access web API with HTTP it redirect to HTTPS
+
+//  Get Contacts Endpoint
+app.MapGet("/api/contacts", async (ApplicationDbContext db) =>
+{
+    var contacts = await db.Contacts.ToListAsync();
+    return Results.Ok(contacts);
+});
 
 app.Run();
